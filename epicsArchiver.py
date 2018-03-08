c7f719d64b806b686d3f9f37418a3285eeadf38b
Gustavo Ciotto <gustavociotto@gmail.com>
2018-03-08 17:04:58 -0300

    initial commit

---
+++ epicsArchiver.py
@@ -0,0 +1,161 @@
+import requests
+import urllib3
+
+class EpicsArchiverRetrieval:    
+
+    def __init__ (self, ipAddress, port = 8080, isSelfSignedCertificate = True, isSSLSecure = True):
+
+        self.ipAddress = ipAddress
+        self.port = port
+        self.isSelfSignedCertificate = isSelfSignedCertificate
+        self.isSSLSecure = self.isSelfSignedCertificate or isSSLSecure
+
+        if isSelfSignedCertificate:
+            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    def getRetrievalData (self) :
+
+        protocol = "http://"
+
+        if self.isSSLSecure:
+            protocol = "https://"
+
+        bpl = self.ipAddress
+
+        if self.port != 8080 and self.port != 80:
+            bpl = self.ipAddress + ":" + str (self.port)
+
+        return protocol + bpl + "/retrieval/data"
+
+    def retrieveData (self, start, end, pvs = []) :
+
+        retrieval = self.getRetrievalData()
+
+        ans = {}
+
+        for pv in pvs:
+
+            requestedPV = pv ["name"]
+
+            if pv ["optimized"] :
+                requestedPV = "optimized_" + str (pv ["bins"]) + "(" + pv ["name"] + ")"
+
+            ans [pv ["name"]] = requests.get(retrieval + "/getData.json", \
+                                    params = {"pv"   : requestedPV, \
+                                              "from" : start.strftime("%Y-%m-%dT%H:%M:%S") + ".000Z", \
+                                              "to"   : end.strftime ("%Y-%m-%dT%H:%M:%S") + ".000Z"}, \
+                                    verify = not self.isSelfSignedCertificate ).json ()
+
+        return ans
+
+    def retrieveMetadata (self, pvs = []):
+
+        retrieval = self.getRetrievalData()
+
+        ans = {}
+
+        for pv in pvs:
+
+            ans [pv ["name"]] = requests.get(retrieval + "/getMetadata.json", \
+                                    params = {"pv"   : requestedPV }, \
+                                    verify = not self.isSelfSignedCertificate ).json ()
+
+        return ans
+
+class EpicsArchiverManagement:
+
+    SUCCESS = 0
+    LOGIN_FAILED = 1
+    UNAUTHENTICATED_USER = 2
+
+    def __init__ (self, ipAddress, port = 8080, requireUserAuthentication = True, isSelfSignedCertificate = True):
+
+        self.ipAddress = ipAddress
+        self.port = port
+        self.requireUserAuthentication = requireUserAuthentication
+        self.isSelfSignedCertificate = isSelfSignedCertificate
+
+        if requireUserAuthentication:
+            self.session = requests.Session ()
+
+        if isSelfSignedCertificate:
+            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    def getMgmtBpl (self) :
+
+        bpl = self.ipAddress
+
+        if self.port != 8080 and self.port != 80:
+            bpl = self.ipAddress + ":" + str (self.port)
+
+        return "https://" + bpl + "/mgmt/bpl"
+
+    def login (self, user, passwd):
+
+        if not self.requireUserAuthentication:
+            return EpicsArchiverManagement.SUCCESS # We don't need to authenticate it
+
+        mgmt = self.getMgmtBpl()
+
+        self.session.post(mgmt + "/login", data = {"username" : user, "password" : passwd}, verify = not self.isSelfSignedCertificate)
+
+        answer = self.session.get (mgmt + "/getLoginUsername",  verify = not self.isSelfSignedCertificate).json()
+
+        if 'username' not in answer or answer['username'] != user:
+            return EpicsArchiverManagement.LOGIN_FAILED
+
+        self.authenticatedUser = user
+
+        return EpicsArchiverManagement.SUCCESS
+
+    def logout (self) :
+
+        if not self.requireUserAuthentication:
+            return EpicsArchiverManagement.SUCCESS  # We don't need to authenticate it
+
+        mgmt = self.getMgmtBpl()
+
+        self.session.post(mgmt + "/logout", verify = not self.isSelfSignedCertificate)
+
+        self.authenticatedUser = None
+
+    def archivePVs (self, pvs = []) :
+
+        if self.requireUserAuthentication and self.authenticatedUser == None:
+            return EpicsArchiverManagement.UNAUTHENTICATED_USER
+
+        mgmt = self.getMgmtBpl()
+
+        for pv in pvs:
+            print "Attempting to archive PV = " + pv ["name"] + " : "
+            print self.session.get(mgmt + "/archivePV", \
+                                   params = {"pv" : pv ["name"], "samplingperiod" : pv ["sampling_period"], "samplingmethod" : pv ["sampling_method"], \
+                                             "policy" : pv ["sampling_policy"]}, \
+                                   verify = not self.isSelfSignedCertificate).json()
+
+    def deletePVs (self, pvs = []) :
+
+        if self.requireUserAuthentication and self.authenticatedUser == None:
+            return EpicsArchiverManagement.UNAUTHENTICATED_USER
+
+        mgmt = self.getMgmtBpl()
+
+        for i in pvs:
+            a = self.session.get(mgmt + "/deletePV", params = {"pv" : i}, verify = not self.isSelfSignedCertificate)
+            print "PV " + i + " : status_code = " + str(a.status_code)
+
+    def pausePVs (self, pvs = []) :
+
+        if self.requireUserAuthentication and self.authenticatedUser == None:
+            return EpicsArchiverManagement.UNAUTHENTICATED_USER
+
+        mgmt = self.getMgmtBpl()
+
+        for i in pvs:
+
+            a = self.session.get(mgmt + "/pauseArchivingPV", params = {"pv" : i}, verify = not self.isSelfSignedCertificate).json()
+
+            if 'validation' in a:
+                print "PV " + i + " : " + a['validation']
+            elif 'engine_desc' in a:
+                print "PV " + i + " : " + a['engine_desc']
---
+++ epicsArchiverWidgets.py
@@ -0,0 +1,241 @@
+# Widgets creation was moved to this module in order to let the notebook cleaner.
+# Gustavo Ciotto Pinton
+
+from ipywidgets import widgets
+
+import epicsArchiver
+import datetime
+import matplotlib
+import time
+import numpy
+
+UTC_OFFSET_TIMEDELTA = time.timezone // 3600
+DELTA = (datetime.datetime (1970, 1, 1, 0, 0, 0) - datetime.datetime (1, 1, 1, 0, 0, 0)).total_seconds();
+
+COLOR_STACK = [ (1.0, 0, 0, 1.0), (0, 1.0, 0, 1.0), (0, 0, 1.0, 1.0), (0, 0, 0, 1.0),(0.9, 0.75, 1.0, 1.0), (0.5, 0, 0, 1.0), (0.67, 0.43, 0.16, 1.0), (0.94, 0.196, 0.90, 1.0) ]
+
+### Event handlers
+
+def removePVHandler (b):
+    PVWidgetList.remove(b.parent)
+    appendPVVBox.children = PVWidgetList + [appendPVWidget]
+
+def appendPVHandler (b):
+    appendPVWidgets ()
+
+def updateBinsText (event):
+    event ["owner"].parent.children [2].disabled = not event ["owner"].parent.children [1].value
+    
+def appendPVWidgets (pv = ""):
+    PVNameWidget = widgets.Text (description = "PV Name:", value = pv)
+
+    PVBinsWidget = widgets.Checkbox(value = False, description = 'Optimized?')
+    PVBinsIntWidget = widgets.IntText (value = "500", description = "# bins:", disabled = not PVBinsWidget.value)
+    PVRemoveWidget = widgets.Button (description = "Remove")
+
+    PVBinsWidget.observe (updateBinsText)
+    PVRemoveWidget.on_click(removePVHandler) 
+
+    newRow = widgets.HBox ([PVNameWidget, PVBinsWidget, PVBinsIntWidget, PVRemoveWidget])
+
+    PVRemoveWidget.parent = newRow
+    PVBinsWidget.parent = newRow
+
+    PVWidgetList.append (newRow)
+
+    appendPVVBox.children = PVWidgetList + [appendPVWidget]
+
+# Encryption 
+def update (sender):
+    isSelfSignedWidget.value = isSSLWidget.value
+    isSelfSignedWidget.disabled = not isSSLWidget.value
+
+### Widgets initialization
+
+# Host IP text widget
+hostIPWidget = widgets.Text (value = "10.0.6.57:8080", description = "IP address:" )
+
+# Encryption preferences widgets - SSL and self-signed certificate
+isSSLWidget = widgets.Checkbox(value = True, description = 'Use HTTPS?')
+isSSLWidget.observe (update)
+
+isSelfSignedWidget = widgets.Checkbox(value = True, description = 'Self-signed SSL?', disabled = not isSSLWidget.value)
+
+encryptionWidgets = widgets.HBox([isSSLWidget, isSelfSignedWidget])
+
+# Datetime widgets
+dateLayout = widgets.Layout(width='25%')
+
+startDateWidget = widgets.DatePicker (description = 'Start: ', layout = dateLayout)
+startHour = widgets.BoundedIntText (min = 0, max = 23, step = 1,description = 'Hour:', layout = dateLayout)
+startMinute = widgets.BoundedIntText (min = 0, max = 59, step = 1, description = 'Minute:', layout = dateLayout)
+startSecond = widgets.BoundedIntText(min = 0, max = 59, step = 1, description = 'Seconds:', layout = dateLayout)
+
+endDateWidget = widgets.DatePicker(description = 'End: ', layout = dateLayout)
+endHour = widgets.BoundedIntText(min = 0, max = 23, step = 1,description = 'Hour:', layout = dateLayout)
+endMinute = widgets.BoundedIntText(min = 0, max = 59, step = 1, description = 'Minute:', layout = dateLayout)
+endSecond = widgets.BoundedIntText(min = 0, max = 59, step = 1, description = 'Seconds:', layout = dateLayout)
+
+dateTimeWidgets = widgets.VBox([widgets.HBox([startDateWidget, startHour, startMinute, startSecond]), \
+                                widgets.HBox([endDateWidget, endHour, endMinute, endSecond])])
+
+# Set default initialization values for the datetime widgets
+now = datetime.datetime.now()
+
+startDateWidget.value = now - datetime.timedelta (minutes = 10)
+startHour.value = startDateWidget.value.hour
+startMinute.value = startDateWidget.value.minute
+startSecond.value = startDateWidget.value.second
+
+endDateWidget.value = now
+endHour.value = now.hour
+endMinute.value = now.minute
+endSecond.value = now.second
+
+# PV field widgets
+appendPVWidget = widgets.Button(description = 'Append')
+appendPVWidget.on_click (appendPVHandler)
+
+PVWidgetList = []
+
+appendPVVBox = widgets.VBox ()
+appendPVWidgets("Cont:LNLS191:NTP:Offset")
+appendPVWidgets("CONT:EVK-M8F:NTP:Offset")
+appendPVWidgets("LNLS:ANEL:corrente")
+appendPVWidgets("PRO:MBTemp:Ch5")
+
+### User helper functions
+
+def drawAddressWidgets ():
+    display (hostIPWidget)
+
+def drawConnectionWidgets ():
+    display (encryptionWidgets)
+
+def drawDatetimeWidgets ():
+    display (dateTimeWidgets)
+
+def drawVariableWidgets ():
+    display (appendPVVBox)
+
+def drawPlot (variables = [], groups = None):
+
+    if  variables == None or (type(variables) == list and len (variables) == 0):
+        return
+
+    if type (variables) == dict:
+        variables = [variables]
+
+    r = epicsArchiver.EpicsArchiverRetrieval (ipAddress = getIP (), port = getPort (), \
+                                              isSSLSecure = isSSL (), \
+                                              isSelfSignedCertificate = isSelfSignedCert ())
+
+    answer = r.retrieveData (start = startTime (), end = endTime (), pvs = variables)
+
+    fig, ax = matplotlib.pyplot.subplots(1, sharex = True, figsize = (12,8))
+
+    init_ax = ax
+
+    units = {}
+    lines = []
+    labels = []
+
+    r = 0
+
+    for i, pv in enumerate(variables):
+
+        x_axis = []
+        y_axis = []
+
+        for sample in answer[pv ["name"]][0]["data"]:
+            # https://matplotlib.org/devdocs/api/_as_gen/matplotlib.pyplot.plot_date.html
+            # matplotlib.pyplot.plot_date(x, y)
+            # x and/or y can be a sequence of dates represented as float days since 0001-01-01 UTC.
+            x_axis.append((DELTA + sample["secs"] - UTC_OFFSET_TIMEDELTA * 3600 + sample["nanos"] * 1e-9) / (24.0 * 60.0 * 60.0 ))
+            y_axis.append(sample["val"])
+
+        if "EGU" in answer[pv ["name"]][0]["meta"].keys():
+            unit = answer[pv ["name"]][0]["meta"]["EGU"]
+        else:
+            unit = pv ["name"]
+
+        if unit in units.keys ():
+            ax = units [unit]
+
+        else :
+            if i == 0:
+                units [unit] = ax
+            else:
+                units [unit] = init_ax.twinx ()
+                ax = units [unit]
+                ax.spines['right'].set_position(('axes', 1 + r))
+
+                r += 0.125
+
+            ax.set_ylabel (unit)
+
+        lines += ax.plot_date (x_axis, y_axis, linestyle = "solid", drawstyle = "steps-post", marker = "", label = pv ["name"], color = COLOR_STACK [i])
+        labels.append (pv ["name"])
+
+    for i, ax in enumerate (units.values ()):
+
+        ax.grid ()
+
+        ax.set_yticks(numpy.linspace(ax.get_ybound()[0], ax.get_ybound()[1], 7))
+
+        ax.xaxis_date ()
+        ax.xaxis.set_major_locator(matplotlib.dates.MinuteLocator())
+        ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H:%M:%S"))
+
+    fig.tight_layout()
+
+    matplotlib.pyplot.legend (lines, labels, loc = 0)
+    matplotlib.pyplot.show (block = False)
+
+def getIP ():
+    return hostIPWidget.value.split (":") [0]
+
+def getPort ():
+    return int(hostIPWidget.value.split (":") [1])
+
+def isSSL ():
+    return isSSLWidget.value
+
+def isSelfSignedCert ():
+    return isSelfSignedWidget.value
+
+def startTime ():
+    start = startDateWidget.value
+    return start.replace (hour = int(startHour.value + UTC_OFFSET_TIMEDELTA), minute = int(startMinute.value), second = int(startSecond.value))
+
+def endTime ():
+    end = endDateWidget.value
+    return end.replace (hour = int(endHour.value + UTC_OFFSET_TIMEDELTA), minute = int(endMinute.value), second = int(endSecond.value))
+
+def getVariables ():
+    pvs = []
+
+    for pvW in PVWidgetList:
+        if pvW.children [0].value != "":
+            pvs.append ({"name" : pvW.children [0].value, "optimized" : pvW.children [1].value, "bins" : pvW.children [2].value})
+
+    return pvs
+
+def getIthVariable (i = 0):
+
+    if i < len (PVWidgetList) and PVWidgetList[i].children [0].value != "":
+        return {"name" : PVWidgetList[i].children [0].value, "optimized" : PVWidgetList[i].children [1].value, "bins" : PVWidgetList[i].children [2].value}
+
+    return None
+
+def getRangeVariables (r = range (0,2)):
+
+    pvs = []
+
+    for i in r:
+        pv = getIthVariable(i)
+        if pv != None:
+            pvs.append (pv)
+
+    return pvs
+
